fe2789927573abe6f3a6f1c20b19357766ce56a2
Digger <dimiatar.sasehv@gmail.com>
2014-09-07 23:13:21 +0300

    Adding support of edit -fix title

--- src/Digger/Icard/GalleryBundle/Controller/DefaultController.php
+++ src/Digger/Icard/GalleryBundle/Controller/DefaultController.php
@@ -104,10 +104,10 @@ class DefaultController extends Controller
         $em = $this->getDoctrine()->getManager();
         
         if ($id) {
-            $title = 'Image upload';
+            $title = 'Image edit';
             $image = $em->getRepository('DiggerIcardGalleryBundle:Image')->find($id);
         } else {
-            $title = 'Image edit';
+            $title = 'Image upload';
             $image = new Image();
             $image->setTitle('Default title');
             $image->setNote('Totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?');
